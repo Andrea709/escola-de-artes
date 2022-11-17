5ed19d401b25f366248fddb4ae27b77b421f5d12
nog <nogueiraandrea816@gmail.com>
2022-11-17 14:42:02 -0300

    get and set
    mais um pouco do controle, mas n mto

---
+++ src/controle/AlunoControle.java
@@ -0,0 +1,17 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Aluno;
+
+public class AlunoControle {
+	
+	private ArrayList<Aluno> tabelaAlunos; //pra que serve?
+	
+	public AlunoControle() {
+		this.tabelaAlunos = new ArrayList<>(); //pra que serve tbm jesus
+	}
+	
+	//INSERT
+	
+}
---
+++ src/modelo/Aluno.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Aluno {
+	
+	private String nome;
+	private Integer telefone;
+	private Long cpf;
+	private String email;
+	
+	public Aluno() {}
+	//public Aluno(String nome, email, Long cpf, Integer telefone) 
+	
+	
+	//getters and setters
+	public String getNome() {
+		return nome;
+	}
+	
+}
