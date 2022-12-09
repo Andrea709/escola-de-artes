138fba1f2e6c5e8e475034c67c62a4d6126163fd
Nog <nogueiraandrea816@gmail.com>
2022-12-09 10:53:46 -0300

    coisas menu aluno

--- src/visao/MenuAluno.java
+++ src/visao/MenuAluno.java
@@ -37,10 +37,12 @@ public class MenuAluno extends JFrame {
 	 */
 	public MenuAluno() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 448, 217);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(211, 211, 211));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLocationRelativeTo(null); //deixar sempre no centro
+
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -52,7 +54,33 @@ public class MenuAluno extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JButton btnCursos = new JButton("Cursos");
-		btnCursos.setBounds(39, 71, 89, 32);
+		btnCursos.setBackground(new Color(128, 128, 128));
+		btnCursos.setBounds(32, 71, 104, 32);
 		contentPane.add(btnCursos);
+		
+		JButton btnNotas = new JButton("Notas");
+		btnNotas.setBackground(new Color(128, 128, 128));
+		btnNotas.setBounds(165, 71, 104, 32);
+		contentPane.add(btnNotas);
+		
+		JButton btnNovoCurso = new JButton("Novo curso");
+		btnNovoCurso.setBackground(new Color(128, 128, 128));
+		btnNovoCurso.setBounds(300, 71, 104, 32);
+		contentPane.add(btnNovoCurso);
+		
+		JButton btnFechar = new JButton("Fechar");
+		btnFechar.setBackground(new Color(128, 128, 128));
+		btnFechar.setBounds(300, 126, 104, 32);
+		contentPane.add(btnFechar);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setBackground(new Color(128, 128, 128));
+		btnVoltar.setBounds(165, 126, 104, 32);
+		contentPane.add(btnVoltar);
+		
+		JButton btnInformacoes = new JButton("Informações");
+		btnInformacoes.setBackground(new Color(128, 128, 128));
+		btnInformacoes.setBounds(32, 126, 104, 32);
+		contentPane.add(btnInformacoes);
 	}
 }
